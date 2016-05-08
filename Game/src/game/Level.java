e605fb175d254aca144bdeb616ef69c8158be82b
fkam <fkam@tkh.se>
2016-05-08 20:18:16 +0200

    Minor change in Level.

--- Game/src/game/Level.java
+++ Game/src/game/Level.java
@@ -13,27 +13,13 @@ public class Level {
 		this.tilemap = tilemap;
 		
 	}
-	
+
 	public void draw(Graphics g){
-		
-		int x = 0;
-		int y = 0;
-		int k = 344;
-		for(int j = 0; j < tiles[1].length; j++){
-			for(int i = 0; i < tiles.length; i++){
-				if (x>=6){
-					x=0;
-					k += 1;
-				}
-				tilemap.drawImage(g, i*32, j*32, k);
-				x++;
+		for(int x = 0; x < tiles.length; x++){
+			for(int y = 0; y < tiles.length; y++){
+				tilemap.drawImage(g, x*32, y*32, tiles[x][y]);
 			}
-			if (y > 5){
-				y = 0;
-				k += 2;
-			}
-			y++;
-		
+				
 		}
 	}
 	
