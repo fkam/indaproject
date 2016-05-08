fce3666492965568050fa7ece4b90180e7415c6c
theagentd <theagentd@hotmail.com>
2016-05-08 16:33:06 +0200

    Added Level class skeleton.

---
+++ Game/src/game/Level.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Level {
+	
+	private Tilemap tilemap;
+	private int[][] tiles;
+	private boolean[][] walkable;
+	
+	public Level(Tilemap tilemap, int[][] tiles, boolean[][] walkable) {
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+	public boolean canWalk(int x, int y){
+		
+	}
+	
+}
